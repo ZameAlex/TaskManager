2f828ce2ae29a85de3e0576da93e52c2f716018c
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-08-31 00:33:11 +0300

    Some changes

--- TaskManagerAPI/Startup.cs
+++ TaskManagerAPI/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using TaskManager.DAL.Interfaces;
+using TaskManager.DAL.UnitOfWork;
 using TaskManager.DLL.DBContext;
 
 namespace TaskManagerAPI
@@ -28,9 +24,9 @@ namespace TaskManagerAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            var connectionString = Configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<TaskManagerContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<TaskManagerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddSingleton<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
