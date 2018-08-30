77bfbec51c891604673b7753ca1fa84e570f545a
Alex <Alex@DESKTOP-3MOEDP1>
2018-08-31 00:44:53 +0300

    Tests project added

---
+++ TaskManager.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TaskManager.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Tst()
+        {
+            Assert.AreEqual(0, 0);
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
