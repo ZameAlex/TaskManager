d43cc670d7bd973351521e32a22919b1897b68cf
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-09-29 20:35:16 +0300

    Small changes in mapping

--- TaskManager.BLL/Mapping/Mapping.cs
+++ TaskManager.BLL/Mapping/Mapping.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TaskManager.BLL.Interfaces;
 using TaskManager.DLL.Models;
 using TaskManager.DLL.Repositories;
