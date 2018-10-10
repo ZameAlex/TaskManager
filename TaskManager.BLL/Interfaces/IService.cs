a23b7010a47dd75ba3cbac0c0723d5c018e6d084
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-10-10 15:18:38 +0300

    BaseService and IService were created

---
+++ TaskManager.BLL/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManager.BLL.Interfaces
+{
+    public interface IService<TEntity>
+    {
+        List<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        int Create(TEntity entity);
+
+        void Update(int id, TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void DeleteById(int id);
+    }
+}
---
+++ TaskManager.BLL/Services/BaseService.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using TaskManager.BLL.Interfaces;
+using TaskManager.BLL.Validation;
+using TaskManager.DLL.Interfaces;
+using TaskManager.DLL.Models;
+
+namespace TaskManager.BLL.Services
+{
+    public class BaseService<TEntity, REntity> : IService<TEntity> where TEntity : class where REntity : Entity
+    {
+        private readonly IRepository<REntity> repository;
+        private readonly IMapper mapper;
+        private readonly IValidator<REntity> validator;
+
+        public BaseService(IRepository<REntity> repository, IMapper mapper, ValidatorFactory validatorFactory)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+            this.validator = validatorFactory.GetValidator<REntity>();
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
