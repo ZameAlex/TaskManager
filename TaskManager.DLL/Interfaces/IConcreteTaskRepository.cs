c9c13e6a0af8ee507efa28f6ae42ae627beb149c
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-10-25 12:26:42 +0300

    Interfaces for TaskRepo were added

---
+++ TaskManager.DLL/Interfaces/IConcreteTaskRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManager.DLL.Models;
+using TaskManager.Shared.Enums;
+
+namespace TaskManager.DLL.Interfaces
+{
+    public interface IConcreteTaskRepository
+    {
+        void UpdateConcreteTaskEstimation(int id, TimeSpan estimation);
+
+        void UpdateConcreteTaskRequirement(int id, bool requirement);
+
+        void UpdateConcreteTaskStartTime(int id, DateTime startTime);
+
+        void UpdateConcreteTaskFrequency(int id, Frequency frequency);
+
+        void UpdateConcreteTaskDaysByAddingDay(int id, CalendarDay day);
+
+        void UpdateConcreteTaskDaysByAddingDays(int id, List<CalendarDay> days);
+
+        void UpdateConcreteTaskDaysByDeletingDay(int id, CalendarDay day);
+
+        void UpdateConcreteTaskDaysByDeletingDays(int id, List<CalendarDay> days);
+
+        void UpdateConcreteTaskStagesByAddingStage(int id, Stage stage);
+
+        void UpdateConcreteTaskStagesByAddingStages(int id, List<Stage> stages);
+
+        void UpdateConcreteTaskStagesByDeletingStage(int id, Stage stage);
+
+        void UpdateConcreteTaskStagesByDeletingStages(int id, List<Stage> stages);
+    }
+}
---
+++ TaskManager.DLL/Interfaces/IGlobalTaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManager.DLL.Models;
+
+namespace TaskManager.DLL.Interfaces
+{
+    public interface IGlobalTaskRepository
+    {
+        void UpdateGlobalTaskEndDate(int id, DateTime endDate);
+
+        void UpdateGlobalTaskTasksByAddingTask(int id, Task task);
+
+        void UpdateGlobalTaskTasksByAddingTasks(int id, List<Task> tasks);
+
+        void UpdateGlobalTaskTasksByDeletingTask(int id, Task task);
+
+        void UpdateGlobalTaskTasksByDeletingTasks(int id, List<Task> tasks);
+    }
+}
